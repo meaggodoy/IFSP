9483b0acb6c5191ab8d810833e0d203b6a052e6b
meag_godoy <meag.godoy@gmail.com>
2026-03-24 22:02:54 -0300

    Remove calculations and input for values and grades
    Removed arithmetic operations and related input/output.

---
+++ Linguagem de Programação/C/AULA2403/EX01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int anoNascimento;
+    
+    printf("Digite o ano em que voce nasceu: ");
+    scanf("%d", &anoNascimento);
+    
+    printf("Bem-vindo ao C! Voce nasceu em %d", anoNascimento);
+    
+    return 0;
+}
