795c6781ccee2b95811df866baef82c359ee136d
meag_godoy <meag.godoy@gmail.com>
2026-03-24 22:11:30 -0300

    Create EX05.c

---
+++ Linguagem de Programação/C/AULA2403/EX05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    
+    if (num % 2 == 0) {
+        printf("O numero eh PAR");
+    } else {
+        printf("O numero eh IMPAR");
+    }
+    
+    return 0;
+}
