0ad6a877481206e26d4ef4ada6f84132b0432dab
meag_godoy <meag.godoy@gmail.com>
2026-03-30 21:33:20 -0300

    Create EX09.c

---
+++ Linguagem de Programação/C/AULA3003/EX09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	  int n1, n2;
+    float media;
+    
+    printf("Digite as duas notas: ");
+    scanf("%d %d", &n1, &n2);
+    
+    media = (float)(n1 + n2)/2;
+    
+    printf("%.2f", media);
+
+    return 0;
+}
