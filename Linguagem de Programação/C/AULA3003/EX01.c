e1d7a01e04d649a3c776e19a7062763751fd5e50
meag_godoy <meag.godoy@gmail.com>
2026-03-30 21:11:43 -0300

    Add program to calculate double of a number

---
+++ Linguagem de Programação/C/AULA3003/EX01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    
+    printf("Seu dobro eh %d", num * 2);
+
+    return 0;
+}
