0c9bbcd46bf642a3f12506fd4f9e47398b9a7583
meag_godoy <meag.godoy@gmail.com>
2026-04-06 19:55:40 -0300

    Add program to check if a number is positive, negative, or zero

---
+++ Linguagem de Programação/C/AULA0604/EX02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    
+    if (num > 0) {
+        printf("Positivo");
+    } else if (num < 0) {
+        printf("Negativo");
+    } else {
+        printf("Zero");
+    }
+
+    return 0;
+}
