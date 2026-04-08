180294443794e91f22bedcfc859cd82a176abb76
meag_godoy <meag.godoy@gmail.com>
2026-04-07 21:18:09 -0300

    Add program to check if a number is even or odd

---
+++ Linguagem de Programação/C/AULA0704/EX04.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    
+    if (num % 2 == 0) {
+        printf("Par");
+    } else {
+        printf("Impar");
+    }
+
+    return 0;
+}
