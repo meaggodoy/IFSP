7f84fe160378744aa891361a45f4e427df8fa556
meag_godoy <meag.godoy@gmail.com>
2026-04-10 16:59:15 -0300

    Add factorial calculation program in EX26.c
    Implement factorial calculation with a loop.

---
+++ Linguagem de Programação/C/AULA0704/EX26.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int num, i, fatorial = 1;
+	
+	printf("Digite um numero: ");
+	scanf("%d", &num);
+	
+	for (i = num; i > 0; i--) {
+		if (num > 1 && i > 1) {
+			fatorial = fatorial * num * (i - 1);
+			num = num - 2;
+			i--;
+		}	
+	}
+	
+	printf("%d", fatorial);
+}
