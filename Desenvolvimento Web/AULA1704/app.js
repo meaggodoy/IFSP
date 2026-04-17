0c44ee30ca6fa5a31d71d6f12ff0c4d71d3de565
meag_godoy <meag.godoy@gmail.com>
2026-04-17 20:26:55 -0300

    Add files via upload

---
+++ Desenvolvimento Web/AULA1704/app.js
@@ -0,0 +1,19 @@
+let nome = "Maria";
+const anoNascimento = 2005;
+let anoAtual = 2026;
+
+let idade = anoAtual - anoNascimento;
+alert("Sua idade é aproximadamente " + idade);
+
+
+let idadeCliente = 15;
+if (idadeCliente >= 18) {
+    console.log("Entrada liberada.");
+} else {
+    console.log("Entrada bloqueada.");
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Contador: " + i);
+}
---
+++ Desenvolvimento Web/AULA1704/desafio.js
@@ -0,0 +1,5 @@
+const botao = document.querySelector('#btn-dark');
+botao.onclick = function() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
