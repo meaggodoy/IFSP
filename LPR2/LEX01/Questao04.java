47c93ba96119de511843b6c882b21a0d29e3a647
meag_godoy <meag.godoy@gmail.com>
2026-08-13 17:04:46 -0300

    Add Questao04.java to demonstrate type casting

---
+++ LPR2/LEX01/Questao04.java
@@ -0,0 +1,10 @@
+public class Questao04 {
+	public static void main(String[] args) {
+		int inteiro = 15;
+		float flutuante;
+		
+		flutuante = (float)inteiro;
+        
+        System.out.println("O flutuante eh: " + flutuante);
+	}
+}
