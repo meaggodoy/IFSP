b8d5eeed1aeefaf346506b9a220a1c568d972e96
meag_godoy <meag.godoy@gmail.com>
2026-08-13 17:03:22 -0300

    Add Questao03.java for type casting example

---
+++ LPR2/LEX01/Questao03.java
@@ -0,0 +1,10 @@
+public class Questao03{
+	public static void main(String[] args) {
+		int inteiro;
+		float flutuante = 15.0f;
+		
+		inteiro = (int)flutuante;
+        
+        System.out.println("O inteiro eh: " + inteiro);
+	}
+}
