4cb9af7c9b821ed59d22cb11bba9598ca026f2f4
meag_godoy <meag.godoy@gmail.com>
2026-08-13 17:06:48 -0300

    Add Questao05.java to demonstrate type casting

---
+++ LPR2/LEX01/Questao05.java
@@ -0,0 +1,10 @@
+public class Questao05 {
+	public static void main(String[] args) {
+		int inteiro;
+		double duble = 17.8;
+		
+		inteiro = (int)duble;
+        
+        System.out.println("O inteiro eh: " + inteiro);
+	}
+}
