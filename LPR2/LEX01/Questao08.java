0a6c01cab883f75f31a8e6d3170bbf69c23c9493
meag_godoy <meag.godoy@gmail.com>
2026-08-13 17:50:05 -0300

    Add Questao08.java with string array example

---
+++ LPR2/LEX01/Questao08.java
@@ -0,0 +1,13 @@
+public class Questao08 {
+	public static void main(String[] args) {
+		String[] vetor = new String[3];
+		
+		vetor[0] = "Antônio da Silva";
+		vetor[1] = "Maria Eduarda";
+		vetor[2] = "Carlos Ricardo";
+		
+        for (int i = 0; i < 3; i++) {
+		    System.out.println("Posicao " + i + ": " + vetor[i]);
+		}
+	}
+}
