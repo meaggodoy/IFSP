5362044a3e128594103c39d7bcfa68a4a62d25b0
meag_godoy <meag.godoy@gmail.com>
2026-08-27 21:39:47 -0300

    Add Teste.java with main method and class usage

---
+++ LPR2/AULA2708/ListaEX/Teste.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        MinhaClasse mc  = new MinhaClasse();
+        
+        mc.metodo1();
+        mc.metodo2(2);
+    } 
+}
