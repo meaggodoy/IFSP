0bb13890885c64635974a1b16aaa1fcc8fc44394
meag_godoy <meag.godoy@gmail.com>
2026-08-27 22:04:27 -0300

    Add MedidorTemperatura class for temperature measurement

---
+++ LPR2/AULA2708/ListaEX/MedidorTemperatura.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class MedidorTemperatura {
+    private float temperatura;
+    
+    public void setTemperatura(float temperatura) {
+        this.temperatura = temperatura;
+    }
+    public void getTemperatura() {
+        return temperatura;
+    }
+}
