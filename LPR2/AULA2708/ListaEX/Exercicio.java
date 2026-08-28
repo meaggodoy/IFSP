d10fa11a3ce605049158193f2d4b23c6a12a0866
meag_godoy <meag.godoy@gmail.com>
2026-08-27 21:52:49 -0300

    Add Exercicio class with two methods

---
+++ LPR2/AULA2708/ListaEX/Exercicio.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class Exercicio {
+    public void metodo1() {
+        System.out.println("Executando exercicio...");;
+    }
+    
+    public void metodo2(int n) {
+        n = n + 20;
+        System.out.println("Valor: " + n);
+    }
+}
