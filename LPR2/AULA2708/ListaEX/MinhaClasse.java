bf28405d5265ccc5079f854563d96c3c63e21873
meag_godoy <meag.godoy@gmail.com>
2026-08-27 21:40:12 -0300

    Add MinhaClasse with two methods

---
+++ LPR2/AULA2708/ListaEX/MinhaClasse.java
@@ -0,0 +1,11 @@
+package teste;
+
+public class MinhaClasse {
+    public void metodo1() {
+        System.out.println("Execucao do metodo 01...");
+    }
+    
+    public void metodo2(int valor) {
+        System.out.println("Valor: " + valor);
+    }
+}
