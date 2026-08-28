f10502206a3f917bd58300d063267677876e66e3
meag_godoy <meag.godoy@gmail.com>
2026-08-27 21:52:30 -0300

    Add Principal class with main method

---
+++ LPR2/AULA2708/ListaEX/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Exercicio ex = new Exercicio();
+        
+        ex.metodo1();
+        ex.metodo2(5);
+    }
+}
