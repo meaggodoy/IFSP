cd6d0a3a891224ab73870c8cfd015976e97eb4b3
meag_godoy <meag.godoy@gmail.com>
2026-09-03 19:40:58 -0300

    Add Cliente details display in Principal.java

--- LPR2/AULA2708/ListaEX/Principal.java
+++ LPR2/AULA2708/ListaEX/Principal.java
@@ -15,5 +15,15 @@ public class Principal {
         MedidorTemperatura mt = new MedidorTemperatura();
         mt.setTemperatura(26.0f);
         JOptionPane.showMessageDialog(null, "A temperatura armazenada foi: " + mt.getTemperatura());
+
+        Cliente c = new Cliente();
+        c.setNome("Maria Eduarda");
+        c.setEndereco("Monte Mor");
+        c.setTelefone(999999999);
+        c.setEmail("teste@teste.br");
+        JOptionPane.showMessageDialog(null, "Nome: " + c.getNome());
+        JOptionPane.showMessageDialog(null, "Endereco: " + c.getEndereco());
+        JOptionPane.showMessageDialog(null, "Telefone: " + c.getTelefone());
+        JOptionPane.showMessageDialog(null, "Email: " + c.getEmail());
     }
 }
