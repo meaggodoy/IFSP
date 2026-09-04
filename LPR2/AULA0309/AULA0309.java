81df82db70850f3e7edfd1a65490c6366c7a0b64
meag_godoy <meag.godoy@gmail.com>
2026-09-03 21:30:24 -0300

    Add AULA0309.java with main method and Tela instance

---
+++ LPR2/AULA0309/AULA0309.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula0309;
+
+/**
+ *
+ * @author ht3078531
+ */
+public class AULA0309 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+        tela.setVisible(true);
+    }
+    
+}
